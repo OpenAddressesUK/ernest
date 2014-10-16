4c2d73ff114d2fa450aabf2ff3f29003493c8db7
pezholio <pezholio@gmail.com>
2014-10-16 14:45:48 +0100

    Change tag types

--- spec/address_spec.rb
+++ spec/address_spec.rb
@@ -4,10 +4,10 @@ describe "Address" do
 
   before do
     @postcode = Tag.create(label: "postcode")
-    @town = Tag.create(label: "town")
+    @town = Tag.create(label: "post_town")
     @locality = Tag.create(label: "locality")
     @street = Tag.create(label: "street")
-    @pao = Tag.create(label: "pao")
+    @building_no = Tag.create(label: "building_no")
   end
 
   it "should create a basic address" do
@@ -16,16 +16,16 @@ describe "Address" do
     address.geo_objects << GeoObject.create(label: "The Shire", tag: @town)
     address.geo_objects << GeoObject.create(label: "Hobbitton", tag: @locality)
     address.geo_objects << GeoObject.create(label: "Hobbit Drive", tag: @street)
-    address.geo_objects << GeoObject.create(label: "3", tag: @pao)
+    address.geo_objects << GeoObject.create(label: "3", tag: @building_no)
 
     address.save
 
     expect(address.geo_objects.count).to eq(5)
     expect(address.postcode.label).to eq('ABC 123')
-    expect(address.town.label).to eq('The Shire')
+    expect(address.post_town.label).to eq('The Shire')
     expect(address.locality.label).to eq('Hobbitton')
     expect(address.street.label).to eq('Hobbit Drive')
-    expect(address.pao.label).to eq('3')
+    expect(address.building_no.label).to eq('3')
   end
 
 end
