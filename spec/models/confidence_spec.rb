8fd5377e5e60ed51389f1e7c270e56256ea62d5e
James Smith <james@floppy.org.uk>
2015-02-26 15:18:33 +0000

    formatting

--- spec/models/confidence_spec.rb
+++ spec/models/confidence_spec.rb
@@ -4,7 +4,7 @@ describe Confidence do
 
   context "testing the basics" do
 
-    before do
+    before :each do
       # Make a pair of tags
       pc_type = FactoryGirl.create(:tag_type, label: "postcode")
       town_type = FactoryGirl.create(:tag_type, label: "town")
@@ -63,7 +63,7 @@ describe Confidence do
     end
 
     context "with a load of data" do
-      before :all do
+      before :each do
         35.times do |n|
           FactoryGirl.create(:address, tags: [
             FactoryGirl.create(:tag, label: "SW1A 1AA", tag_type: FactoryGirl.create(:tag_type, label: "postcode")),
@@ -121,15 +121,15 @@ describe Confidence do
       {matching: 470, total: 473, confidence: 0.9545},
       {matching: 454,	total: 454,	confidence: 0.9539},
       {matching: 403,	total: 407,	confidence: 0.9508},
-      {matching: 4,	total: 407,	confidence: 0.000},
+      {matching: 4,	  total: 407,	confidence: 0.000},
       {matching: 87,	total: 87,	confidence: 0.8935},
       {matching: 560,	total: 563,	confidence: 0.9584},
-      {matching: 3,	total: 563,	confidence: 0.000},
+      {matching: 3,	  total: 563,	confidence: 0.000},
       {matching: 333,	total: 335,	confidence: 0.9459},
-      {matching: 1,	total: 335,	confidence: 0.000},
-      {matching: 1,	total: 335,	confidence: 0.000},
+      {matching: 1,	  total: 335,	confidence: 0.000},
+      {matching: 1,	  total: 335,	confidence: 0.000},
       {matching: 101,	total: 103,	confidence: 0.9004},
-      {matching: 2,	total: 103,	confidence: 0.000},
+      {matching: 2,	  total: 103,	confidence: 0.000},
       {matching: 129,	total: 129,	confidence: 0.9129},
       {matching: 34,	total: 34,	confidence: 0.8257},
       {matching: 60,	total: 60,	confidence: 0.8709},
@@ -141,11 +141,11 @@ describe Confidence do
       {matching: 49,	total: 49,	confidence: 0.8565},
       {matching: 78,	total: 78,	confidence: 0.8873},
       {matching: 686,	total: 689,	confidence: 0.9624},
-      {matching: 3,	total: 689,	confidence: 0.000},
+      {matching: 3,	  total: 689,	confidence: 0.000},
       {matching: 25,	total: 25,	confidence: 0.7940},
       {matching: 83,	total: 91,	confidence: 0.8859},
-      {matching: 1,	total: 91,	confidence: 0.000},
-      {matching: 7,	total: 91,	confidence: 0.000},
+      {matching: 1,	  total: 91,	confidence: 0.000},
+      {matching: 7,	  total: 91,	confidence: 0.000},
       {matching: 22,	total: 25,	confidence: 0.7660},
     ].each do |data|
       
