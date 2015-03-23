c53347a6894abd345bd08f89c23105d6e780dbd5
pezholio <pezholio@gmail.com>
2015-03-23 09:31:17 +0000

    No, this is not a red herring

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -49,7 +49,7 @@ RSpec.configure do |config|
 
   config.before(:suite) do
     DatabaseCleaner.clean_with :truncation
-    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.strategy = :truncation
   end
 
   config.around(:each) do |example|
