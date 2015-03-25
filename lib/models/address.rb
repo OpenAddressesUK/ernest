a8aa5466828d048d1aed5dd7cdea3d431ef9532c
pezholio <pezholio@gmail.com>
2015-03-25 13:26:37 +0000

    Create confidence for user submitted addresses

--- lib/models/address.rb
+++ lib/models/address.rb
@@ -26,7 +26,7 @@ class Address < ActiveRecord::Base
   end
 
   def heuristic_adjustment(confidence)
-    t = (Date.today - (valid_at || created_at).to_date).to_f / 365.242 
+    t = (Date.today - (valid_at || created_at || Date.today).to_date).to_f / 365.242
     hl = 15
     decay = 0.5 ** (t / hl)
     confidence * decay
