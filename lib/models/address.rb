184fe93347ffa7cc68e37e48bb758ce76bcba69f
pezholio <pezholio@gmail.com>
2015-04-20 13:48:55 +0100

    Don't generate score each time

--- lib/models/address.rb
+++ lib/models/address.rb
@@ -6,8 +6,6 @@ class Address < ActiveRecord::Base
   has_and_belongs_to_many :tags
   belongs_to :user
 
-  after_create :generate_score
-
   TagType::ALLOWED_LABELS.each do |label|
     define_method(label) do
       tags.select { |g| g.tag_type.label == label }.first
